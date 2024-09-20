abc3e16e7c29a01c65ccb2426714009533092e84
KumarDalvi <122657286+KumarDalvi@users.noreply.github.com>
2024-09-20 20:50:17 +0530

    addedd hospital website all code

---
+++ index.js
@@ -0,0 +1,13 @@
+gsap.to(".hosp_para"),
+  {
+    rotation: 27,
+    x: 100,
+    duration: 1,
+  };
+
+gsap.to(".footer_opyrights", { 
+    rotation: 360, 
+    x: 100,
+    duration: 1 
+    
+});
